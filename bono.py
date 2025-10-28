643bc15301b55835eda266aa6f6b6184e47c030c
Loreta <macerdava@gmail.com>
2025-10-28 19:31:28 -0300

    feat(add menu)

--- bono.py
+++ bono.py
@@ -5,3 +5,38 @@ def calcular_bono(sueldo_base, porcentaje_bono):
     sueldo_final = sueldo_base + bono
     print ("Cálculo realizado.")
     return bono, sueldo_final
+
+def mensaje_despedida():
+    print("Saliendo del programa... ¡Hasta luego!")
+
+
+# Menú de usuario
+
+def menu():
+
+    nombre = edad = cargo = ""
+    sueldo_base = porcentaje_bono = bono = sueldo_final = 0
+
+    while True:
+        print("\n=== Bienvenido a PyCompany ===")
+        print("1. Ingresar datos del empleado")
+        print("2. Calcular bono")
+        print("3. Mostrar resumen")
+        print("4. Salir")
+
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            print("Opción 1 seleccionada: Ingresar datos del empleado")
+            nombre, edad, cargo, sueldo_base, porcentaje_bono = ingresar_datos_usuario()
+        elif opcion == "2":
+            print("Opción 2 seleccionada: Calcular bono") 
+            bono, sueldo_final = calcular_bono(sueldo_base, porcentaje_bono)
+        elif opcion == "3":
+            print("Opción 3 seleccionada: Mostrar resumen")  
+            mostrar_resultados(nombre, edad, cargo, sueldo_base, porcentaje_bono, bono, sueldo_final)
+        elif opcion == "4":
+            mensaje_despedida()
+            break  # rompe el ciclo while y termina el menú
+        else:
+            print("Opción inválida. Intente nuevamente.")
+menu()
